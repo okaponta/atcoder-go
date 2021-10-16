d3d45b8b6d7675f8decfbc9d5eb19dbc1df23484
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-10-16 22:38:19 +0900

    refactor. make code shorter.

--- abc/abc222/c/swiss-system-tournament.go
+++ abc/abc222/c/swiss-system-tournament.go
@@ -27,70 +27,44 @@ type Player struct {
 }
 
 func jj(a, b byte) int {
-	if a == 'G' {
-		if b == 'C' {
-			return 1
-		}
-		if b == 'P' {
-			return -1
-		}
+	if a == b {
 		return 0
 	}
-	if a == 'C' {
-		if b == 'P' {
-			return 1
-		}
-		if b == 'G' {
-			return -1
-		}
-		return 0
+	if a == 'G' && b == 'C' || a == 'C' && b == 'P' || a == 'P' && b == 'G' {
+		return 1
 	}
-	if a == 'P' {
-		if b == 'G' {
-			return 1
-		}
-		if b == 'C' {
-			return -1
-		}
-		return 0
-	}
-	return 0
+	return -1
 }
 
 func main() {
 	sc.Split(bufio.ScanWords)
 	defer wr.Flush()
 	n, m := nextInt(), nextInt()
-	playerNum := n * 2
-	janken := make([][]byte, playerNum)
+	janken := make([][]byte, 2*n)
 	for i := range janken {
 		janken[i] = nextBytes()
 	}
-	leaderBoard := make([]Player, playerNum)
-	for i := range leaderBoard {
-		leaderBoard[i] = Player{i, 0}
+	rank := make([]Player, 2*n)
+	for i := range rank {
+		rank[i] = Player{i, 0}
 	}
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
-			a := leaderBoard[2*j]
-			b := leaderBoard[2*j+1]
-			aj := janken[a.num][i]
-			bj := janken[b.num][i]
-			match := jj(aj, bj)
+			match := jj(janken[rank[2*j].num][i], janken[rank[2*j+1].num][i])
 			if match == 1 {
-				leaderBoard[2*j].win++
+				rank[2*j].win++
 			} else if match == -1 {
-				leaderBoard[2*j+1].win++
+				rank[2*j+1].win++
 			}
 		}
-		sort.Slice(leaderBoard, func(i, j int) bool {
-			if leaderBoard[i].win == leaderBoard[j].win {
-				return leaderBoard[i].num < leaderBoard[j].num
+		sort.Slice(rank, func(i, j int) bool {
+			if rank[i].win == rank[j].win {
+				return rank[i].num < rank[j].num
 			}
-			return leaderBoard[i].win > leaderBoard[j].win
+			return rank[i].win > rank[j].win
 		})
 	}
-	for _, p := range leaderBoard {
+	for _, p := range rank {
 		fmt.Fprintln(wr, p.num+1)
 	}
 }
