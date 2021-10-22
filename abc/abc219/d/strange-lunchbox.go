c9f31aae5de8da9c05542d50489c494945f281ff
Kohei Okamoto <okaponta.19910919@gmail.com>
2021-10-22 23:50:30 +0900

    wip

---
+++ abc/abc219/d/strange-lunchbox.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+type bento struct {
+	tako, tai int
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	n, x, y := nextInt(), nextInt(), nextInt()
+
+	count := make([][][]int, n+1)
+	for i := 0; i <= n; i++ {
+		count[i] = make([][]int, x+1)
+		for j := 0; j <= x; j++ {
+			count[i][j] = make([]int, y+1)
+			for k := 1; k <= y; k++ {
+				count[i][j][k] = 2147483647
+			}
+		}
+	}
+	count[0][0][0] = 0
+	for i := 1; i <= n; i++ {
+		a, b := nextInt(), nextInt()
+		for j := 0; j <= x; j++ {
+			for k := 0; k <= y; k++ {
+				count[i][j][k] = min(count[i][j][k], count[i-1][j][k])
+				if count[i-1][j][k] == 1e9 {
+					continue
+				}
+				jj := min(j+a, x)
+				kk := min(k+b, y)
+				count[i][jj][kk] = min(count[i][jj][kk], count[i-1][j][k]+1)
+			}
+		}
+	}
+	if count[n][x][y] == 2147483647 {
+		count[n][x][y] = -1
+	}
+	fmt.Println(count[n][x][y])
+}
